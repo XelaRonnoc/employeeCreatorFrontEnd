43c8b9166e3944440a2001cab0f9253a394a1005
Alex <alexcmilton@gmail.com>
2023-06-05 12:20:33 +1000

    test setup complete

--- employeeCreatorWebApp/vite.config.ts
+++ employeeCreatorWebApp/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    test: {
+        globals: true, // so we don't need to import describe and it every time
+        environment: "jsdom",
+        setupFiles: "./config/setup-tests.js", // path to the config file
+    },
 });
